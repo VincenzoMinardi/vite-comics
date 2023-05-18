2dd701fc848bf22c3b92533ea0ef727fe0144f6f
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-18 17:14:33 +0200

    completed main

--- src/components/PMain.vue
+++ src/components/PMain.vue
@@ -2,8 +2,57 @@
 </script>
 
 <template>
+   <div class="main">
+        <h2 class="text"> Content goes here</h2>
+    </div>
+    <div class="bar-main">
+        <div class="cont-img">
+            <img class="img-main" src="../assets/img/buy-comics-digital-comics.png" alt="">
+            <h5>DIGITAL COMICS</h5>
+        </div>
+        <div class="cont-img">
+            <img class="img-main" src="../assets/img/buy-comics-merchandise.png" alt="">
+            <h5>DC MERCHANDISE</h5>
+        </div>
+        <div class="cont-img">
+            <img class="img-main" src="../assets/img/buy-comics-subscriptions.png" alt="">
+            <h5>SUBSCRIPTION</h5>
+        </div>
+        <div class="cont-img">
+            <img class="img-main" src="../assets/img/buy-comics-shop-locator.png" alt="">
+            <h5>COMIC SHOP LOCATOR</h5>
+        </div>
+        <div class="cont-img">
+            <img class="img-main" src="../assets/img/buy-dc-power-visa.svg" alt="">
+            <h5>DC POWER VISA</h5>
+        </div>
+    </div>
+   
+    
+    
  
 </template>
 
 <style lang="scss" scoped>
+@use './../assets/styles/partials/variables' as *;
+
+.main{
+    @include color-main;
+}
+
+.bar-main{
+    @include mini-main;
+}
+
+.text{
+    @include color-text-main;
+}
+
+.img-main{
+    @include img-main;
+}
+.cont-img{
+    @include container;
+}
+
 </style>
