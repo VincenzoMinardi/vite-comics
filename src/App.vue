a7c36b9aaec76e772aa3f8529d7697f69f95e0ec
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-18 15:02:32 +0200

    add: image

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import PHeader from './components/PHeader.vue'
+import PMain from './components/PMain.vue'
+import PFooter from './components/PFooter.vue'
+
+export default{
+  components:{
+    PHeader,
+    PMain,
+    PFooter,
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ <PHeader />
+ <PMain />
+ <PFooter />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
 </style>
